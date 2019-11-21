a6cdd5f4321c7d8b1141d2fc1d3f9f19014a37ca
Antabla <tonytafut@gmail.com>
2019-11-21 15:57:15 -0500

    Add files via upload

---
+++ src/app/config/page-menus.ts
@@ -0,0 +1,25 @@
+var pageMenus = [{
+  'icon': 'fa fa-th-large',
+  'title': 'Dashboard',
+  'url': '',
+  'caret': 'true',
+  'submenu': [{
+    'url': 'dashboard/v1',
+    'title': 'Dashboard v1'
+  }, {
+    'url': 'dashboard/v2',
+    'title': 'Dashboard v2'
+  }, {
+    'url': 'dashboard/v3',
+    'title': 'Dashboard v3'
+  }]
+},
+ {
+   'icon':'fa -fa-th-large',
+   'title':'Registro de usuarios',
+   'url': 'registroUsuarios',
+   'caret': 'true'
+ }
+];
+
+export default pageMenus;
